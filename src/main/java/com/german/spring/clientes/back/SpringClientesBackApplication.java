53202ecbac271d7c2bd05de26f4bc449bf61b484
German Sanchez Pastor <german.sanchezpastor@innotec.security>
2021-02-16 13:16:17 +0100

    Subiendo codigo

---
+++ src/main/java/com/german/spring/clientes/back/SpringClientesBackApplication.java
@@ -0,0 +1,13 @@
+package com.german.spring.clientes.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClientesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/german/spring/clientes/back/SpringClientesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.german.spring.clientes.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
