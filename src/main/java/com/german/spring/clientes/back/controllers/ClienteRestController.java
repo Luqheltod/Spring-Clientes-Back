244d6e63d66c9226fbf7cfbcdd8d90e8b5e6296b
German Sanchez Pastor <luqh555@gmail.com>
2021-02-17 12:03:49 +0100

    Se añade corssorigin

--- src/main/java/com/german/spring/clientes/back/controllers/ClienteRestController.java
+++ src/main/java/com/german/spring/clientes/back/controllers/ClienteRestController.java
@@ -3,12 +3,14 @@ package com.german.spring.clientes.back.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.german.spring.clientes.back.model.Cliente;
 import com.german.spring.clientes.back.service.ClienteService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
